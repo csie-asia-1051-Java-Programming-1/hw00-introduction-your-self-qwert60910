aa985744809e719509e9a5170de4880f56995a82
qwert60910 <105021042@live.asia.edu.tw>
2016-09-12 15:49:01 +0800

    hw-001

---
+++ hw001/src/hw001.java
@@ -0,0 +1,14 @@
+
+public class hw001 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("§Ú¬O¬h©ö©_");
+
+	}
+
+}
